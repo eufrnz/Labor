e398cd004e3ce4395bc29e897928bc85ef3749d3
Aluno <aluno@IPIRANGA.FATEC>
2026-09-09 17:13:46 -0300

    test

--- src/main/java/br/net/labor/service/CandidateInJobService.java
+++ src/main/java/br/net/labor/service/CandidateInJobService.java
@@ -2,6 +2,7 @@ package br.net.labor.service;
 
 import br.net.labor.model.candidateApplication.ApplicationStatus;
 import br.net.labor.model.candidateApplication.CandidateApplication;
+import br.net.labor.model.dto.jobs.JobsVacanciesResponseWithOutCandidatesDTO;
 import br.net.labor.model.dto.likeJobs.LikeInJobsResponseDTO;
 import br.net.labor.model.jobs.JobVacancies;
 import br.net.labor.model.typeUser.Candidate;
@@ -11,6 +12,8 @@ import br.net.labor.repository.JobVacanciesRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 @Service
@@ -43,6 +46,25 @@ public class CandidateInJobService {
                 jobVacancies.getTitle(),
                 candidate.getUsername()
         );
+
+    }
+
+    public List<JobsVacanciesResponseWithOutCandidatesDTO> myLikes(String email, UUID id){
+        List<CandidateApplication> applications = applicationRepository.findAll();
+        JobVacancies jobVacancies = jobVacanciesRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Vacancy not found"));
+//        return new JobsVacanciesResponseWithOutCandidatesDTO(
+//                jobVacancies.getId(),
+//                jobVacancies.getTitle(),
+//                jobVacancies.getAbility(),
+//                jobVacancies.getPayValue(),
+//                jobVacancies.getInitTime(),
+//                jobVacancies.getEndTime(),
+//                jobVacancies.getDateJob(),
+//                jobVacancies.getDescription(),
+//                jobVacancies.getCompany().getCompanyName()
+//        );
+        return null;
 
     }
 
